c466eb855381779392a4240de24f60953c8d02d2
tkmyers46 <tkmyers46@msn.com>
2018-06-08 22:40:13 -0700

    Update PasswordChange.ps1
    replace details with your info

--- ActiveDirectory/PasswordChange.ps1
+++ ActiveDirectory/PasswordChange.ps1
@@ -3,11 +3,11 @@
 ## Install-Module -name AD-Domain-Services
 Import-Module activedirectory
 
-$oldPassword = ConvertTo-SecureString 'Almond2016' -AsPlainText -Force
-$newPassword = ConvertTo-SecureString 'Peanut2016' -AsPlainText -Force
-$domain = ConvertTo-SecureString 'fareast.corp.microsoft.com' -AsPlainText -Force
-Set-ADAccountPassword -Identity pftst3a -OldPassword $oldPassword -NewPassword $newPassword
-Set-ADAccountPassword -Server $domain -Identity pftst3a -OldPassword $oldPassword -NewPassword $newPassword
-Set-ADAccountPassword -Identity pftst3a -OldPassword $oldPassword -NewPassword $newPassword -Partition 'DC=fareast, DC=corp, DC=microsoft, DC=com'
+$oldPassword = ConvertTo-SecureString '<oldpassword>' -AsPlainText -Force
+$newPassword = ConvertTo-SecureString '<newpassword>' -AsPlainText -Force
+$domain = ConvertTo-SecureString 'domain.contoso.com' -AsPlainText -Force
+Set-ADAccountPassword -Identity useralias -OldPassword $oldPassword -NewPassword $newPassword
+Set-ADAccountPassword -Server $domain -Identity useralias -OldPassword $oldPassword -NewPassword $newPassword
+Set-ADAccountPassword -Identity useralias -OldPassword $oldPassword -NewPassword $newPassword -Partition 'DC=domain, DC=contoso, DC=com'
 Get-Help Set-ADAccountPassword -Examples
-Set-ADAccountPassword 'DC=fareast, DC=corp, DC=microsoft, DC=com' -Identity acu523f -OldPassword $oldPassword -NewPassword $newPassword
+Set-ADAccountPassword 'DC=domain, DC=contoso, DC=com' -Identity useralias -OldPassword $oldPassword -NewPassword $newPassword
