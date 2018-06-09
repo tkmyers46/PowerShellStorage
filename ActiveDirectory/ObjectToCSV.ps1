062093c9d2a38cd6de83226a9821526c65e48636
tkmyers46 <tkmyers46@msn.com>
2018-06-08 22:44:42 -0700

    Update ObjectToCSV.ps1

--- ActiveDirectory/ObjectToCSV.ps1
+++ ActiveDirectory/ObjectToCSV.ps1
@@ -1,7 +1,7 @@
-﻿cd C:\Users\trmye\Documents\Temp 
-get-aduser v-setho | Export-CSV 'C:\Users\trmye\OneDrive - Microsoft 1\Scripts\ActiveDirectory\ExportedObjectsCSV\ExportObject.csv'
-Get-ADUser -filter {(mail -eq "*") -and (sn -eq "Smith")} | Export-CSV 'C:\Users\trmye\OneDrive - Microsoft 1\Scripts\ActiveDirectory\ExportedObjectsCSV\ExportObject.csv'
-Get-ADUser -Filter 'Name -like "v-setho"' | Export-CSV 'C:\Users\trmye\OneDrive - Microsoft 1\Scripts\ActiveDirectory\ExportedObjectsCSV\ExportObject.csv'
-Get-ADObject -Filter 'Name -like "v-setho"' -Server 'redmond.corp.microsoft.com'
-Get-ADObject -Filter 'Name -like "v-setho"' -Searchbase 'DC=redmond,DC=corp,DC=microsoft,DC=com' | 
-Get-ADObject -Filter 'Name -like "v-setho"' -Searchbase 'OU=UserAccounts,DC=redmond,DC=corp,DC=microsoft,DC=com' | Export-CSV 'C:\Users\trmye\OneDrive - Microsoft 1\Scripts\ActiveDirectory\ExportedObjectsCSV\ExportObject.csv'
+﻿ 
+get-aduser useralias | Export-CSV 'C:\filepath\ExportObject.csv'
+Get-ADUser -filter {(mail -eq "*") -and (sn -eq "lastname")} | Export-CSV 'C:\filepath\ExportObject.csv'
+Get-ADUser -Filter 'Name -like "useralias"' | Export-CSV 'C:\filepath\ExportObject.csv'
+Get-ADObject -Filter 'Name -like "useralias"' -Server 'domain.contoso.com'
+Get-ADObject -Filter 'Name -like "useralias"' -Searchbase 'DC=domain,DC=contoso,DC=com' 
+Get-ADObject -Filter 'Name -like "useralias"' -Searchbase 'OU=UserAccounts,DC=domain,DC=contoso,DC=com' | Export-CSV 'C:\filepath\ExportObject.csv'
